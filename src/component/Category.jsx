d09b3ea0633b966d533b6ae84a14cc2fffcbe492
Usama Ahmad <usamaathar32@gmail.com>
2025-07-30 11:57:38 +0500

    Fix: correct Category component path

--- src/component/Category.jsx
+++ src/component/Category.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import Category from "./Category";
 import cat1 from "../assets/cat1.jpg";
 import cat2 from "../assets/cat2.jpg";
 import cat3 from "../assets/cat3.jpg";

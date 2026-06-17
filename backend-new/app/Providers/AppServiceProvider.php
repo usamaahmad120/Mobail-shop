f57f56f59fcabb4a29f1f72c455c44d4cdce7ce3
Usama Ahmad <usamaathar32@gmail.com>
2026-06-17 12:25:29 +0500

    lastest code

--- backend-new/app/Providers/AppServiceProvider.php
+++ backend-new/app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
+use Livewire\Mechanisms\FrontendAssets\FrontendAssets;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Livewire::setScriptRoute(function ($handle) {
+            return Route::get('/livewire/livewire.js', $handle);
+        });
+
+        Route::get('/livewire.js', [FrontendAssets::class, 'returnJavaScriptAsFile']);
     }
 }
